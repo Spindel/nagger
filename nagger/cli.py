8e1367e13af84be12abb2ea5cbe8deef0f263666
D.S. Ljungmark <spider@skuggor.se>
2020-03-28 00:36:24 +0100

    Rebuild the commandline UI with Click
    This splits the "bot" (CI) usage and the "release" usage into different
    modules, and adds another module for the CLI, splitting it out to make it
    easier to reuse the pieces.
    
    The logging was updated to use `contextvars` in modern structlog, and the CLI
    can also ask about the milestone to use.

--- nagger/cli.py
+++ nagger/cli.py
@@ -1,68 +1,106 @@
 #!/usr/bin/env python3
 """Simple cli part for nagger"""
 import os
+import sys
 
 import click
-from . import (
-    mr_nag,
-    release_tag,
-    milestone_changelog,
-    milestone_fixup,
-    milestone_release,
-    setup_logging,
-)
+from . import ci_bot
+from . import release
+
+import structlog.contextvars
+
+log = structlog.get_logger()
+
+
+def setup_logging():
+    """Global state. Eat it"""
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.StackInfoRenderer(),
+            structlog.dev.set_exc_info,
+            structlog.processors.format_exc_info,
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S", utc=False),
+            structlog.dev.ConsoleRenderer(colors=True),
+        ],
+        logger_factory=structlog.PrintLoggerFactory(sys.stderr),
+    )
+    structlog.contextvars.clear_contextvars()
+    log.debug("Logging, debug, initialized")
+    log.msg("log.msg initialized")
+
+
+def _prompt_milestone(milestone: str) -> str:
+    """Helper for interactive use of prompting for milestone."""
+    if milestone:
+        return milestone
+    ok_milestones = release.get_milestones()
+    choice = click.Choice(ok_milestones)
+    milestone = click.prompt("Pick milestone", show_choices=True, type=choice)
+    return milestone
+
+
+@click.group()
+def bot():
+    """Bot (CI) commands"""
+    setup_logging()
+
+
+@bot.command()
+def debug_variables():
+    ci_bot.debug_variables()
+
+
+@bot.command()
+def nag():
+    """Merge request nagger. meant to be run in a CI job"""
+    ci_bot.mr_nag()
+
+
+@bot.command()
+def tag_to_release():
+    """Turn a tag to a release object."""
+    ci_bot.release_tag()
 
 
 @click.group()
 @click.pass_context
-def cli(ctx):
+def milestone(ctx):
     """Main command"""
     setup_logging()
-    global _log
 
-    if ctx.invoked_subcommand != "debug-variables":
-        if "NAGGUS_KEY" not in os.environ:
-            ctx.fail("We expect NAGGUS_KEY environment variable to contain an API key")
+    if "NAGGUS_KEY" not in os.environ:
+        ctx.fail("We expect NAGGUS_KEY environment variable to contain an API key")
 
 
-@cli.command()
-def debug_variables():
-    debug_variables()
-
-
-@cli.command()
-@click.option("-n", "--dry-run", is_flag=True)
-def nag(dry_run):
-    """Merge request nagger. meant to be run in a CI job"""
-    mr_nag()
-
-
-@cli.command()
-@click.option("-n", "--dry-run", is_flag=True)
-def make_release(dry_run):
-    release_tag()
-
-
-@cli.command()
-@click.argument("milestone")
+@milestone.command()
+@click.argument("milestone", required=False)
 def changelog(milestone):
     """Generate changelog for milestone"""
-    milestone_changelog(milestone)
+    setup_logging()
+    milestone = _prompt_milestone(milestone)
+    release.milestone_changelog(milestone)
 
 
-@cli.command()
+@milestone.command()
 @click.option("-n", "--dry-run", is_flag=True)
-@click.argument("milestone")
-def fixup(dry_run, milestone):
+@click.argument("milestone", required=False)
+@click.pass_context
+def fixup(ctx, dry_run, milestone):
     """Stomp all over the milestone and attempt to fix Merge requests and
     issues."""
-    milestone_fixup(milestone, dry_run)
+    setup_logging()
+    milestone = _prompt_milestone(milestone)
+    release.milestone_fixup(milestone, dry_run)
 
 
-@cli.command()
+@milestone.command()
 @click.option("-n", "--dry-run", is_flag=True)
 @click.argument("tag-name")
 def tag_release(dry_run, tag_name):
     """Try to tag all projects involved with the milestone."""
     assert tag_name.count(".") >= 2, "A full tag name, eg v3.15.0"
-    milestone_release(tag_name, dry_run)
+    release.milestone_release(tag_name, dry_run)
+
+
+cli = click.CommandCollection(sources=[milestone, bot])
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["python-gitlab", "structlog", "colorama", "python-dateutil",
+requirements = ["python-gitlab", "structlog>==20.1.0.", "colorama", "python-dateutil",
                 "click"]
 
 setup_requirements = []
