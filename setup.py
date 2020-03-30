33e9de4ba115a9cb3d9931083b6072fb879c707a
D.S. Ljungmark <spider@skuggor.se>
2020-03-30 16:35:16 +0200

    setup: Bump required versions
    This bumps some version numbers semi randomly

--- setup.py
+++ setup.py
@@ -11,8 +11,13 @@ with open("README.md") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["python-gitlab", "structlog>==20.1.0.", "colorama", "python-dateutil",
-                "click"]
+requirements = [
+    "python-gitlab >= 2.1.2",
+    "structlog >= 20.1",
+    "colorama",
+    "python-dateutil",
+    "click >= 7.1",
+]
 
 setup_requirements = []
 
@@ -41,7 +46,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://gitlab.com/Spindel/nagger",
-    version="0.1.0",
+    version="0.2.0",
     zip_safe=False,
     entry_points={"console_scripts": ["nagger = nagger.cli:cli"]},
 )
